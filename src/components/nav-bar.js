ef2c365733a5abbbecc936ad09fb1652e78af3b8
Luc <8822552+luc-github@users.noreply.github.com>
2020-03-09 11:03:00 +0100

    Update nav-bar.js
    Show hide a panel content using click ( not best way but faster for the moment)

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -19,11 +19,11 @@ export default () => {
         <h1>ESP3D</h1>
         <version><a href="https://github.com/luc-github/esp3d-webui" target="_blank" ><Github/>v3.0</a></version>
         <nav>
-          <NavItem linkTo="/" text="Dashboard" icon={Server}/>
+        <Link className="nav-link" href="/" id="dash" ><Server size={24}/> Dashboard</Link>
           <NavItem linkTo="/camera" text="Camera" icon={Video}/>
           <NavItem linkTo="/settings" text="Settings" icon={Settings}/>
-          <Link href="/login"  className={showit?"visible":"invisible"} ><User size={24}/> Login</Link>
-          <Link onClick={() => {setshow(!showit); console.log("click")}}><button class="btn btn-dark" type="button">{showit?"hide":"show"}</button></Link>
+          <Link className="nav-link" href="/login"  className={showit?"nav-link visible":"nav-link invisible"} ><User size={24}/> Login</Link>
+          <Link className="nav-link" onClick={() => {setshow(!showit); console.log("click");document.getElementById("dash").click();}}><button class="btn btn-dark" type="button">{showit?"hide":"show"}</button></Link>
         </nav>
     </header>
     </div>
